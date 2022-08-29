33b3b8b40288a87749e8415fb6d56599f86fa711
Da_Vinc4i <da_vinc4i_play@gmail.com>
2022-08-29 06:33:04 +0300

    First commit

---
+++ HomeWork_6/Func2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6
+
+{
+    class Func2
+    {
+        public static double Quardratic(double x)
+        {
+            return 3 * x * x;
+        }
+        public static double Linear(double x)
+        {
+            return 2 * x + 3;
+        }
+        public static double Squaring(double x)
+        {
+            return x * x;
+        }
+        public static double Cubing(double x)
+        {
+            return x * x * x;
+        }
+        public static double Sqrt(double x)
+        {
+            return Math.Sqrt(x);
+        }
+        public static double Sin(double x)
+        {
+            return Math.Sin(x);
+        }
+    }
+}
---
+++ HomeWork_6/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using HomeWork;
+
+namespace HomeWork_6
+{
+    // 1.Изменить программу вывода таблицы функции так, чтобы можно было передавать функции
+    // типа double (double, double). Продемонстрировать работу на функции с функцией a*x^2 и
+    // функцией a*sin(x).
+
+
+    // 2. Модифицировать программу нахождения минимума функции так, чтобы можно было
+    // передавать функцию в виде делегата.
+    // а) Сделать меню с различными функциями и представить пользователю выбор, для какой
+    // функции и на каком отрезке находить минимум. Использовать массив (или список) делегатов,
+    // в котором хранятся различные функции.
+
+    // решение предоставил Юрченко Н.А.
+
+    public delegate double Fun(double x);
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var screen = new Screen();
+            screen.ScreenPrint(6,1);
+            // Вызов метода ScreenPrint cо второй домашней работы, для вывода номера задания 
+            // и домашней работы, а также вывода ФИО выполнившего студента
+
+            List<Fun> functions = new List<Fun> { new Fun(Func2.Quardratic), new Fun(Func2.Linear), new Fun(Func2.Squaring), new Fun(Func2.Cubing), new Fun(Func2.Sqrt), new Fun(Func2.Sin) };
+            Console.WriteLine("Выберите номер функции");
+            Console.WriteLine("1. f(x) = a * x^2 (3 * x^2)");
+            Console.WriteLine("2. f(x) = k * x + b (2 * x + 3)");
+            Console.WriteLine("3. f(x) = x^2");
+            Console.WriteLine("4. f(x) = x^3");
+            Console.WriteLine("5. f(x) = x^1/2");
+            Console.WriteLine("6. f(x) = Sin(x)\n");
+
+            Console.Write("Номер функции:  ");
+            int Choose = Get.GetInt(functions.Count);
+
+            Console.WriteLine();
+            Console.Write("Для нахождения минимума введите отрезок в формате х0 хn:  ");
+
+            double start = 0;
+            double end = 0;
+            Get.GetInterval(out start, out end);
+
+            Console.WriteLine();
+            Console.Write("Введите размер шага:  ");
+            double step = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Get.SaveFunc("data.bin", start, end, step, functions[Choose - 1]);
+            double min = double.MaxValue;
+
+            Console.WriteLine();
+            Console.WriteLine("Получены следующие значения функции:\n");
+            Table2.Table(start, end, step, Get.Load("data.bin", out min));
+
+            Console.WriteLine();
+            Console.WriteLine("Минимальное значение функции: {0:0.00}", min);
+            Console.ReadKey();
+        }
+    }
+}
